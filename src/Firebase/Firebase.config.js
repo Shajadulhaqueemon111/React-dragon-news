659563e9ec8f3812e8aa04199e68439c51343640
Md Emon <mdshajdulhaqueemon@gmail.com>
2023-10-05 02:12:29 +0600

    allprovider

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -4,14 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 
-console.log('inside firebase config',import.meta.env.firebaseConfig)
+console.log('inside firebase config',import.meta.env.VITE_APIKE)
 
 const firebaseConfig = {
   apiKey:           import.meta.env. VITE_APIKEY ,
   authDomain:       import.meta.env. VITE_AUTHDOMAIN ,
   projectId:        import.meta.env. VITE_PROJECTID  ,
-  storageBucket:    import.meta.env.VITE_STORAGEBUCKET    ,
-  messagingSenderId:import.meta.env. VITE_MESSAGINGSENDERID   ,
+  storageBucket:    import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env. VITE_MESSAGINGSENDERID,
   appId:import.meta.env. VITE_APPID, 
 };
 
